38f59a1079711047193781ee0b7f6e4a3165d509
Ahmad Azri <azrirazak01@gmail.com>
2021-05-17 09:04:30 +0800

    Manage file

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 
 Telegram Bot For Text Classification
 Using Bayes Method
+
+# installation
+* Clone the repo into your machine
+* Get a bot using (BotFather)[https://t.me/botfather] to get your token
+* Rename ```.env.example``` to ```.env``` and paste your token
+* run ```npm install```
+
+# run
+* ```npm start```
+
+# method
+* Implementation text classification at ```./src/service/index.js```
+* Model at ```./src/service/model/model.json```
+* Implementation bot at ```./src/app.js```
---
+++ src/app.js
@@ -0,0 +1,22 @@
+const { Telegraf } = require('telegraf');
+const { prediction } = require('./services');
+const { positive, negative }  = require('./services/model/classification').text;
+require('dotenv').config();
+
+const bot = new Telegraf(process.env.TELE_TOKEN);
+
+bot.start( (ctx) => ctx.reply('Welcome To Tweet Disaster Classification'));
+bot.help((ctx) => ctx.reply('Will guess if the text enter are disaster tweet or not'));
+
+bot.on('text', async (ctx) => {
+    const { text } = ctx.message;
+    text.trim() === '' && ctx.reply('Enter a statement about disaster');
+    const result = await prediction(text.trim());
+    const reply = result === positive ? 'Something bad is happening' : 'Its normal'; 
+    ctx.reply(reply);
+});
+
+bot.launch();
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
